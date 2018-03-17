016a6add1aaedcd789489206e003d81d9ca15809
Filip Salamic <fsalamic16@gmail.com>
2018-03-18 00:32:38 +0100

    initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,23 @@
+# Inherit from common
+include device/samsung/j5x-common/BoardConfigCommon.mk
+
+LOCAL_PATH := device/samsung/j5xnlte
+
+# Asserts
+TARGET_OTA_ASSERT_DEVICE := j5xnlte,j5xnltexx
+
+# Kernel
+TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_j5xnlte_eur_defconfig
+
+# Audio
+AUDIO_FEATURE_SAMSUNG_DUAL_SIM := true
+
+# Radio
+SIM_COUNT := 2
+
+# Partition sizes
+BOARD_SYSTEMIMAGE_PARTITION_SIZE    := 3145728000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 12138278912
+
+# RIL
+BOARD_GLOBAL_CFLAGS += -DRIL_FIX_SMS_NOT_SENT_ERR
---
+++ device.mk
@@ -0,0 +1,36 @@
+#Inherit from vendor
+$(call inherit-product-if-exists, vendor/samsung/j5xnlte/j5xnlte-vendor.mk)
+
+# Inherit from common
+$(call inherit-product, device/samsung/j5x-common/device-common.mk)
+
+LOCAL_PATH := device/samsung/j5xnlte
+
+# System properties
+-include $(LOCAL_PATH)/system_prop.mk
+
+# Common overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+#NFC
+#PRODUCT_PACKAGES += \
+#	android.hardware.nfc@1.0-impl \
+#	android.hardware.nfc@1.0-service \
+#	com.android.nfc_extras \
+#	libnfc_nci \
+#	libnfc_nci_jni \
+#	nfc_nci.pn54x.msm8916 \
+#	NfcNci \
+#	Tag
+
+# NFC configs
+#PRODUCT_COPY_FILES += \
+#	$(LOCAL_PATH)/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml \
+#	$(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
+#	$(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf
+
+# Permissions
+#PRODUCT_COPY_FILES += \
+#	frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+#	frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+#	frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml
